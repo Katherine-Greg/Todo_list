02343ec4e65e0d63b96c3d7f142c1fe6ecbb16b5
Katherine-Greg <katiasimpson17@gmail.com>
2023-11-30 16:07:38 +0200

    Update models, changed urls.py & views.py

--- todo_list_service/urls.py
+++ todo_list_service/urls.py
@@ -1,11 +1,25 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from todo_list_service.views import TaskListView, TagListView
+from todo_list_service.views import (TaskListView,
+                                     TaskCreateView,
+                                     TagListView,
+                                     TagUpdateView,
+                                     TagCreateView,
+                                     TagDeleteView,
+                                     TaskUpdateView,
+                                     TaskDeleteView,
+                                     change_status,)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/change-status", change_status, name="change-status"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo_list"
